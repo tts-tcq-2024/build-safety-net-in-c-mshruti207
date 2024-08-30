db170ca82a2869be50209b5231062bd8e9a9d1a4
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-08-30 11:18:18 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,72 @@
-#include <gtest/gtest.h>
-#include "Soundex.h"
+#include <check.h>
+#include <string.h>
+#include "soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+// Test case for the Soundex algorithm
+START_TEST(test_soundex_basic) {
+    char output[5];
+
+    soundex("Smith", output);
+    ck_assert_str_eq(output, "S530");
+
+    soundex("Jackson", output);
+    ck_assert_str_eq(output, "J025");
+
+    soundex("Robert", output);
+    ck_assert_str_eq(output, "R163");
+}
+END_TEST
+
+START_TEST(test_soundex_edge_cases) {
+    char output[5];
+
+    // Test with empty string
+    soundex("", output);
+    ck_assert_str_eq(output, "0000");
+
+    // Test with single letter
+    soundex("A", output);
+    ck_assert_str_eq(output, "A000");
+
+    // Test with all same letters
+    soundex("AAAAA", output);
+    ck_assert_str_eq(output, "A000");
+
+    // Test with mixed letters
+    soundex("McDonald", output);
+    ck_assert_str_eq(output, "M235");
+}
+END_TEST
+
+// Suite setup and teardown
+Suite* soundex_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Soundex");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_soundex_basic);
+    tcase_add_test(tc_core, test_soundex_edge_cases);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+// Main function to run the tests
+int main(void) {
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = soundex_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
