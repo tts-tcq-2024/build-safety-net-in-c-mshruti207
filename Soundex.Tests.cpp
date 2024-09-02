7ea99d9ce4141282592943bd5c11733b72d46383
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-02 21:32:21 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,6 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generate_soundex("AX", soundex);
+  generateSoundex("AX", soundex);
   ASSERT_EQ(soundex,"A200");
 }
