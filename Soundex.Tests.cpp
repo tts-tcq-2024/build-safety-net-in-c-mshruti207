5d9caaacdd269dc70bbf4d10a2d8eec2eaa27175
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-02 21:34:28 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,53 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
-
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
+ 
+//Append 3 zeros if result contains less than 3 digits
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+//AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  ASSERT_EQ(soundex,"A200");
+  ASSERT_STREQ(soundex,"A200");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+//AAA
+  char soundex[5];
+  generateSoundex("A1CDFe", soundex);
+  ASSERT_STREQ(soundex,"A231");
+}
+ 
+//Test case to ckeck special character
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+//AAA
+  char soundex[5];
+  generateSoundex("A[", soundex);
+  ASSERT_STREQ(soundex,"A000");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
+//AAA
+  char soundex[5];
+  generateSoundex("ACGJ", soundex);
+ASSERT_STREQ(soundex,"A200");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
+//AAA
+  char soundex[5];
+  generateSoundex("@@BBCCDD", soundex);
+ASSERT_STREQ(soundex,"@123");
+}
+//Replace all adjacent same digits with one digit
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
+//AAA
+  char soundex[5];
+  generateSoundex("Abfp", soundex);
+  ASSERT_STREQ(soundex,"A100");
+}
+ 
+//two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
+//AAA
+  char soundex[5];
+  generateSoundex("BShZ", soundex);
+  ASSERT_STREQ(soundex,"B200");
 }
