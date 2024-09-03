22eb24d84cd676d9f0ad72609ec370ced814cd5c
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-03 14:00:50 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,14 +4,15 @@
 //Append zeros if result contains less than 3 digits
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
 //AAA
-  char soundex[5];
+  char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("AX", soundex);
   ASSERT_STREQ(soundex,"A200");
 }
- 
+
+//skip numeric and replace consonants with appropriate digit
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
 //AAA
-  char soundex[5];
+  char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("A1CDFe", soundex);
   ASSERT_STREQ(soundex,"A231");
 }
@@ -19,42 +20,29 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
 //Test case to check special character or entire numeric
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
 //AAA
-  char soundex[5];
+ char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("A[", soundex);
   ASSERT_STREQ(soundex,"A000");
-}
-
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
-//AAA
-  char soundex[5];
   generateSoundex("1234", soundex);
-ASSERT_STREQ(soundex,"");
-}
- 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
-//AAA
-  char soundex[5];
+  ASSERT_STREQ(soundex,"000");
   generateSoundex("Shr@12n", soundex);
-ASSERT_STREQ(soundex,"S650");
+  ASSERT_STREQ(soundex,"S650");
 }
 
+//Test case to check special character or numeric at starting
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_51) {
 //AAA
-  char soundex[5];
+ char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("@@[shr12n", soundex);
-ASSERT_STREQ(soundex,"S650");
+  ASSERT_STREQ(soundex,"S650");
+  generateSoundex("12shr3n", soundex);
+  ASSERT_STREQ(soundex,"S650");
 }
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_52) {
-//AAA
-  char soundex[5];
-  generateSoundex("@@[shhr12n", soundex);
-ASSERT_STREQ(soundex,"S650");
-}
 //Replace all adjacent same digits with one digit
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
 //AAA
-  char soundex[5];
+  char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("Abfp", soundex);
   ASSERT_STREQ(soundex,"A100");
 }
@@ -62,13 +50,9 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
 //two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
 //AAA
-  char soundex[5];
+ char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("BShZ", soundex);
   ASSERT_STREQ(soundex,"B200");
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_8) {
-//AAA
-  char soundex[5];
   generateSoundex("lgwxnm", soundex);
   ASSERT_STREQ(soundex,"L250");
 }
@@ -76,7 +60,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_8) {
 //Test case to check Empty 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_9) {
 //AAA
-  char soundex[5];
+  char soundex[MAX_CODE_LENGTH+1];
   generateSoundex("", soundex);
   ASSERT_STREQ(soundex,"");
 }
