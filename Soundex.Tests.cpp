6b47c58a58e700d2c500f18a1fa047afdba76f3f
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-10 13:41:46 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,14 +2,14 @@
 #include "Soundex.h"
 
 // Test fixture for parameterized Soundex tests
-class SoundexTest : public ::testing::TestWithParam<std::soundex<std::string, std::string>> {
+class SoundexTest : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
 };
 
 // Parameterized test case
 TEST_P(SoundexTest, SoundexFunctionTest) {
     std::string input;
     std::string expected;
-    std::output(input, expected) = GetParam();
+    std::tie(input, expected) = GetParam();
 
     char result[5];  // 4 characters + null terminator
     generateSoundex(input.c_str(), result);
@@ -22,16 +22,16 @@ INSTANTIATE_TEST_SUITE_P(
     Default,  // Name of the test suite
     SoundexTest,  // Test fixture class
     ::testing::Values(
-        std::testSoundexPaddingSingleCharWithZeros("A", "A000"),
-        std::testSoundexSkipsNonAlphabeticCharacters("A1CDFe", "A231"),
-        std::testSoundexSkipsSpecialCharacters("B[", "B000"),
-        std::testSoundexSkipsNumericCharacters("1234", "000"),
-        std::testSoundexSkipsSpecialCharacters("Shr@12n", "S650"),
-        std::testSoundexSkipsSpecialCharacters("@@[shr12n", "S650"),
-        std::testSoundexFindsFirstAlphabeticCharacter("12shr3n", "S650"),
-        std::testSoundexReplacesAdjacentSameDigitsOnce("Abfp", "A100"),
-        std::testSoundexSingleCodeForSameDigitsSeparatedBy_HWY("BShZ", "B200"),
-        std::testSoundexEmpty("", "")
+        std::make_tuple("A", "A000"), //testSoundexPaddingSingleCharWithZeros
+        std::make_tuple("A1CDFe", "A231"),//testSoundexSkipsNonAlphabeticCharacters
+        std::make_tuple("B[", "B000"),//testSoundexSkipsSpecialCharacters
+        std::make_tuple("1234", "000"),//testSoundexSkipsNumericCharacters
+        std::make_tuple("Shr@12n", "S650"),//testSoundexSkipsSpecialCharacters
+        std::make_tuple("@@[shr12n", "S650"),//testSoundexSkipsSpecialCharacters
+        std::make_tuple("12shr3n", "S650"),//testSoundexFindsFirstAlphabeticCharacter
+        std::make_tuple("Abfp", "A100"),//testSoundexReplacesAdjacentSameDigitsOnce
+        std::make_tuple("BShZ", "B200"),//testSoundexSingleCodeForSameDigitsSeparatedBy_HWY
+        std::testSoundexEmpty("", "")//testSoundexEmpty
         
     )
 )
