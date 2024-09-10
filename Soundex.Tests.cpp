dc6e0f1c25f74609ac8ae05c7d99c1e8526cb1e4
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-10 15:59:48 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -16,15 +16,15 @@ INSTANTIATE_TEST_SUITE_P(
     SoundexTests,
     SoundexTest,
     ::testing::Values(
-        std::make_tuple("A", "A000"),
-        std::make_tuple("A1CDFe", "A231"),
-        std::make_tuple("B[", "B000"),
-        std::make_tuple("1234", "000"),
-        std::make_tuple("Shr@12n", "S650"),
-        std::make_tuple("@@[shr12n", "S650"),
-        std::make_tuple("12shr3n", "S650"),
-        std::make_tuple("Abfp", "A100"),
-        std::make_tuple("BShZ", "B200"),
-        std::make_tuple("", "")
+        std::make_tuple("A", "A000"),         // test soundex to pad with zeros for single character
+        std::make_tuple("Rohit", "R300"),     // test soundex to replace consonants with digit code  
+        std::make_tuple("A1CDFe", "A231"),    // test soundex to check Non alphabetic character 
+        std::make_tuple("B[", "B000"),        // test soundex to handle special character 
+        std::make_tuple("1234", "000"),       // test soundex to handle Numerics 
+        std::make_tuple("@@[shr12n", "S650"), // test soundex to find first valid alphabetic character
+        std::make_tuple("12shr3n", "S650"),   // test soundex to handle Numerics 
+        std::make_tuple("Abfp", "A100"),      // test soundex to replace adjacent same digit code only once
+        std::make_tuple("BShZ", "B200"),      // test soundex to replace characters with the same number separated by "h", "w" or "y" are coded as a single number
+        std::make_tuple("", "")               //test soundex for empty
     )
 );
