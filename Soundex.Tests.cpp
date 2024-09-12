502955015be1907f2b727d9afb71378a36e612c6
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-12 10:13:30 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,30 +1,66 @@
 #include <gtest/gtest.h>
-#include <cstring>
-#include "Soundex.h"  // Include your Soundex header file
-
-class SoundexTest : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
-};
-
-TEST_P(SoundexTest, CorrectSoundexEncoding) {
-    auto [input, expected] = GetParam();
-    char result[MAX_CODE_LENGTH + 1];
-    generateSoundex(input.c_str(), result);
-    EXPECT_EQ(expected, std::string(result));
+#include "Soundex.h"
+ 
+//Append zeros if result contains less than 3 digits
+TEST(SoudexTestsuite, PadZerosForSingleCharacter) {
+//AAA
+  char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("AX", soundex);
+  ASSERT_STREQ(soundex,"A200");
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    SoundexTests,
-    SoundexTest,
-    ::testing::Values(
-        std::make_tuple("A", "A000"),         // test soundex to pad with zeros for single character
-        std::make_tuple("Rohit", "R300"),     // test soundex to replace consonants with digit code  
-        std::make_tuple("A1CDFe", "A231"),    // test soundex to check Non alphabetic character 
-        std::make_tuple("B[", "B000"),        // test soundex to handle special character 
-        std::make_tuple("1234", "000"),       // test soundex to handle Numerics 
-        std::make_tuple("@@[shr12n", "S650"), // test soundex to find first valid alphabetic character
-        std::make_tuple("12shr3n", "S650"),   // test soundex to handle Numerics 
-        std::make_tuple("Abfp", "A100"),      // test soundex to replace adjacent same digit code only once
-        std::make_tuple("BShZ", "B200"),      // test soundex to replace characters with the same number separated by "h", "w" or "y" are coded as a single number
-        std::make_tuple("", "")               //test soundex for empty
-    )
-);
+//skip numeric and replace consonants with appropriate digit
+TEST(SoudexTestsuite, SkipNumericsAndReplaceConsanantsWithDigitCode) {
+//AAA
+  char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("A1CDFe", soundex);
+  ASSERT_STREQ(soundex,"A231");
+}
+ 
+//Test case to check special character or entire numeric
+TEST(SoudexTestsuite, SkipSpecialCharacterNumericAndReplaceConsanantsWithDigitCode) {
+//AAA
+ char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("A[", soundex);
+  ASSERT_STREQ(soundex,"A000");
+  generateSoundex("1234", soundex);
+  ASSERT_STREQ(soundex,"000");
+  generateSoundex("Shr@12n", soundex);
+  ASSERT_STREQ(soundex,"S650");
+}
+
+//Test case to check special character or numeric at starting
+TEST(SoudexTestsuite, TestCheckSpecialCharacterAndNumericAtStarting) {
+//AAA
+ char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("@@[shr12n", soundex);
+  ASSERT_STREQ(soundex,"S650");
+  generateSoundex("12shr3n", soundex);
+  ASSERT_STREQ(soundex,"S650");
+}
+
+//Replace all adjacent same digits with one digit
+TEST(SoudexTestsuite, TestCheckReplaceAdjacentSameDigitsOnce) {
+//AAA
+  char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("Abfp", soundex);
+  ASSERT_STREQ(soundex,"A100");
+}
+ 
+//two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
+TEST(SoudexTestsuite, TestCheckReplaceWithSinglenumberSeparatedBy_HWY) {
+//AAA
+ char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("BShZ", soundex);
+  ASSERT_STREQ(soundex,"B200");
+  generateSoundex("lgwxnm", soundex);
+  ASSERT_STREQ(soundex,"L250");
+}
+
+//Test case to check Empty 
+TEST(SoudexTestsuite, TestCheckEmptyCharacter) {
+//AAA
+  char soundex[MAX_CODE_LENGTH+1];
+  generateSoundex("", soundex);
+  ASSERT_STREQ(soundex,"");
+}
